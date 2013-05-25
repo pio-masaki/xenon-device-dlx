a2b9c39bca9dbc9277e6e92755787ebf9624f73a
pio_masaki <piomasaki@gmail.com>
2013-05-25 12:43:21 -0700

    Remove kernel lines we don't use or cause issues having here

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,10 +43,8 @@ BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGE_SIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=dlx user_debug=31  
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
-TARGET_KERNEL_VERSION := 3.4
 TARGET_KERNEL_CONFIG := dlx_defconfig
-TARGET_KERNEL_SOURCE := kernel/htc/dlx-$(TARGET_KERNEL_VERSION)
-TARGET_PREBUILT_KERNEL := device/htc/dlx/prebuilt/kernel
+TARGET_KERNEL_SOURCE := kernel/htc/dlx
 
 # We use the old ION API still
 BOARD_HAVE_OLD_ION_API := true
